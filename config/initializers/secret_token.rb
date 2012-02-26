a996190e7990cbd338648f572874afde379f1913
John Mason <macedancer00@gmail.com>
2012-02-25 19:07:40 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewsfeedApp::Application.config.secret_token = '20b141986960edac1775a2846721700a22b5df23641c9f3a48a135f7f6e0b01ba8f03d9efe12f647aab6f970a76a49b09843c00b607c448cf1bd65737968f252'
